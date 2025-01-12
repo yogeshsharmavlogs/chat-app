a0bbbc28f19996f4230400be99f6055c0d3250cd
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-12 23:37:24 +0530

    create passowrd logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,13 @@
 export const signup = (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+    if (password.length < 6) {
+        return res.this.status(400).json({message: "Password must be at least 6 characters"})
+    }
+
+    const user = await User.findOne({email})
+
+    if(user) return res.status(400).json({ message: "Email already exist"})
   } catch (error) {}
 };
 
