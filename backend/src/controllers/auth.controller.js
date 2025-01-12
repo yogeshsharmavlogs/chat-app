a5c81a5206cba2f47e9b08c3789e243013ba0fab
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-12 23:30:04 +0530

    creating schema.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 export const signup = (req, res) => {
-  res.send("signup route");
+  const { fullName, email, password } = req.body;
+  try {
+  } catch (error) {}
 };
 
 export const login = (req, res) => {
