15da2de705bb5d2033f8d5710644d1bfff54bc6d
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-13 23:20:35 +0530

    create hashedpasword

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,6 @@
+import User from "../models/user.model";
+import bcrypt from "bcryptjs"
+
 export const signup = (req, res) => {
   const { fullName, email, password } = req.body;
   try {
@@ -8,6 +11,16 @@ export const signup = (req, res) => {
     const user = await User.findOne({email})
 
     if(user) return res.status(400).json({ message: "Email already exist"})
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+const newUser = new User({
+    fullName: fullName,
+    email: email,
+    password: hashedPassword
+})
+
   } catch (error) {}
 };
 
