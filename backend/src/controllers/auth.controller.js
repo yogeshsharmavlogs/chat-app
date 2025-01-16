622ba69783d85fe06bd24580b46d40bf27047b60
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-16 17:03:04 +0530

    added checkauth and updated code

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -109,3 +109,12 @@ export const updateProfile = async (req, res) => {
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+export const checkAuth = async (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
