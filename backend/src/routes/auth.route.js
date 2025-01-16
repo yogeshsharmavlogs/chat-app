7ce0118c3a2ac38dc35105094e24f0fd66ea5165
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-16 17:03:48 +0530

    added Schema and Routes.

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { login, logout, signup } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { updateProfile } from "../controllers/auth.controller.js";
+import { updateProfile, checkAuth } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -10,5 +10,7 @@ router.post("/login", login);
 router.post("/logout", logout);
 
 router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
