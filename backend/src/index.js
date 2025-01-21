33af07c6ec1eaa252978d33666af531e52018494
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-21 16:46:27 +0530

    install cors in b ackend and use here

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
