c149ada89573e7e80841e5abdc08a7d8cce9048b
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-21 11:20:35 +0530

    created Navbar component.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const { authUser } = useAuthStore();
-  return <div>Navbar</div>;
+  const { logout, authUser } = useAuthStore();
+  return (
+    <header className="bg-base-100 border-b border-base-300 fixed w-full top-0 z-40">
+      <div className="container mx-auto px-4 h-16">
+        <div className="flex items-center justify-between h-full">
+          <div className="flex items-center gap-8">
+            <Link
+              to="/"
+              className="flex items-center gap-2.5 hover:opacity-80 transition-all"
+            >
+              <div className="w-9 h-9 rounded-lg bg-primary/10 flex items-center justify-center">
+                <MessageSquare className="w-5 h-5 text-primary" />
+              </div>
+              <h1 className="text-lg font-bold"> Chatty</h1>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
