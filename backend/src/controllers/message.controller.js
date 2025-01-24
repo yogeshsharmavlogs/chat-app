5f510c3901165daa092d2cf2ad0405f095e37a27
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-24 15:35:48 +0530

    import cloudinary in message.controller.js file

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
