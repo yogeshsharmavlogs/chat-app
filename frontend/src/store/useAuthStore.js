a5aca10ec11430f9a50951439678b6887a1f656d
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-24 15:16:07 +0530

    created sendMessages auth and import socket in useAuthStore.js

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,13 +2,14 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 
-export const useAuthStore = create((set) => ({
+export const useAuthStore = create((set, get) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
   isUpdatingProfile: false,
-
   isCheckingAuth: true,
+  onlineUsers: [],
+  socket: null,
 
   checkAuth: async () => {
     try {
@@ -30,6 +31,7 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.post("/auth/signup", data);
       set({ authUser: res.data });
       toast.success("Account created successfully");
+      get().connectSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -43,6 +45,8 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.post("/auth/login", data);
       set({ authUser: res.data });
       toast.success("Logged in successfully");
+
+      get().connetSocket();
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -73,4 +77,6 @@ export const useAuthStore = create((set) => ({
       set({ isUpdatingProfile: false });
     }
   },
+
+  connectSocket: () => {},
 }));
