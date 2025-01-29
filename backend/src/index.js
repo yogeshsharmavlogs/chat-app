af9157624d227f94f99ea5853cd68952b6f2aca4
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-29 15:54:20 +0530

    updated index.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,16 +3,18 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
-const app = express();
+app;
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
