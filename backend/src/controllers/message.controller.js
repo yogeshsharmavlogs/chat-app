e4ee1e5775bb50820af3f0da7ac48a867b5acf09
yogeshsharmavlogs <yogeshsharmavlogs>
2025-01-29 10:53:31 +0530

    created receiverSocketId

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 import cloudinary from "../lib/cloudinary.js";
+import { io } from "../lib/socket.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -61,6 +62,10 @@ export const sendMessage = async (req, res) => {
     await newMessage.save();
 
     // todo: realtime functionality goes here => socket.io
+    const getReceiverSocketId = getReceiverSocketId(receiverId);
+    if (!receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
 
     res.status(201).json(newMessage);
   } catch (error) {
