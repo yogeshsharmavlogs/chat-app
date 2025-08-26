94eedcecb00e20a63d4573199e7ce6e6cdf9421b
yogeshsharmavlogs <ys9659954@gmail.com>
2025-08-26 10:17:31 +0530

    removed coment from chatHeader page

--- frontend/src/components/ChatHeader.jsx
+++ frontend/src/components/ChatHeader.jsx
@@ -10,7 +10,6 @@ const ChatHeader = () => {
     <div className="p-2.5 border-b border-base-300">
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-3">
-          {/* Avatar */}
           <div className="avatar">
             <div className="size-10 rounded-full relative">
               <img
