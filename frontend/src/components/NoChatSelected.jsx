69e75daf06ec0fba16e3db79d01b433a0169feff
yogeshsharmavlogs <ys9659954@gmail.com>
2025-08-30 11:00:27 +0530

    removed coment from NoChatSelected file

--- frontend/src/components/NoChatSelected.jsx
+++ frontend/src/components/NoChatSelected.jsx
@@ -4,7 +4,6 @@ const NoChatSelected = () => {
   return (
     <div className="w-full flex flex-1 flex-col items-center justify-center p-16 bg-base-100/50">
       <div className="max-w-md text-center space-y-6">
-        {/* Icon Display */}
         <div className="flex justify-center gap-4 mb-4">
           <div className="relative">
             <div
