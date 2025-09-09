eccbbdebda4789915e2a727f7854cf0cfcc524f8
yogeshsharmavlogs <ys9659954@gmail.com>
2025-09-09 11:08:40 +0530

    removed coment line from SidebarSkeleton page.

--- frontend/src/components/skeletons/SidebarSkeleton.jsx
+++ frontend/src/components/skeletons/SidebarSkeleton.jsx
@@ -18,7 +18,6 @@ const SidebarSkeleton = () => {
       <div className="overflow-y-auto w-full py-3">
         {skeletonContacts.map((_, idx) => (
           <div key={idx} className="w-full p-3 flex items-center gap-3">
-            {/* Avatar skeleton */}
             <div className="relative mx-auto lg:mx-0">
               <div className="skeleton size-12 rounded-full" />
             </div>
